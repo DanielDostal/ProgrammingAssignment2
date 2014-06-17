f06af6dac215ae96fb027c6980f3fc65a153f4ca
DanielDostal <daniel.dostal@upol.cz>
2014-06-17 12:59:55 -0700

    My solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functin makeCacheMatrix creates a matrix. You can get inversion of this matrix with cacheSolve function.
+## If you asked for the inversion next time, you obtain cached solution without computing.
+## Example:
+## mat <- matrix(1:4,2,2)
+## special_mat <- makeCacheMatrix(mat)
+## inverse_mat <- cacheSolve(special_mat)
 
-## Write a short comment describing this function
+## This funstion obtains matrix and creates a special matrix (which is in fact a list of 4 functions)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) i <<- inv
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Call this function when you need the inversion of you special matrix created by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
+
+## Note: credit to original creator of makeVector and cacheMean functions; I just replaced several letters
+
